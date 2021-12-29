5a0ffcd46407568d527bbc9aa2bcd0d30d0b8560
Erkan Yalçın <erkyalcin@gmail.com>
2021-12-29 20:28:41 +0300

    formatted file

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
-import {Password} from '../services/password'
+import { Password } from '../services/password'
 //Interface that describes the properties taht are required to create a new User
-interface UserAttrs{
+interface UserAttrs {
     email: string;
     password: string;
 }
@@ -29,28 +29,28 @@ const userSchema = new mongoose.Schema({
         required: true
     }
 },
-{
-    toJSON: {
-        transform(doc, ret){
-            ret.id = ret._id;
-            delete ret._id;
-            delete ret.password;
-            delete ret.__v;
+    {
+        toJSON: {
+            transform(doc, ret) {
+                ret.id = ret._id;
+                delete ret._id;
+                delete ret.password;
+                delete ret.__v;
+            }
         }
-    }
-} );
+    });
 
 userSchema.pre('save', async function (done) {
-    
-    if(this.isModified('password')){
+
+    if (this.isModified('password')) {
         const hashed = await Password.toHash(this.get('password'));
         this.set('password', hashed);
     }
 
     done();
-}); 
+});
 
-userSchema.statics.build = (attrs:UserAttrs) => {
+userSchema.statics.build = (attrs: UserAttrs) => {
     return new User(attrs);
 };
 
